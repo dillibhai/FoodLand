e5fbd1e5ce62cb562bd7babeb1a70239cc80d0b9
sjnyos <wrksuraj@gmail.com>
2020-01-03 11:28:02 +0545

    navigation menu created and working fine

--- app/src/main/java/com/machamasisuraj/foodland/MainActivity.java
+++ app/src/main/java/com/machamasisuraj/foodland/MainActivity.java
@@ -1,14 +1,68 @@
 package com.machamasisuraj.foodland;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private DrawerLayout dl;
+    private ActionBarDrawerToggle t;
+    private NavigationView nv;
+    private TextView tv_textnames;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv_textnames = findViewById(R.id.tv_textnames);
+        dl = findViewById(R.id.activity_main);
+        t = new ActionBarDrawerToggle(this, dl,R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        t.setDrawerIndicatorEnabled(true);
+
+        dl.addDrawerListener(t);
+        t.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        nv = findViewById(R.id.nv);
+        nv.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//                FragmentManager fragmentManager = getSupportFragmentManager();
+//                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+                int id = item.getItemId();
+                dl.closeDrawer(GravityCompat.START);
+                switch(id)
+                {
+                    case R.id.navone:
+                        Toast.makeText(MainActivity.this, "heheh clicked", Toast.LENGTH_SHORT).show();
+                    default:
+                        return true;
+                }
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(t.onOptionsItemSelected(item))
+            return true;
+
+        return super.onOptionsItemSelected(item);
     }
 }
